07466eba4cf83fc4e848a8494bc84341dccf13b2
Dawmuc <vincentpettineo@yahoo.fr>
2020-01-31 10:38:45 +0100

    IA

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Completed
 {
@@ -8,12 +10,13 @@ namespace Completed
 	{
 		public int playerDamage; 							//The amount of food points to subtract from the player when attacking.
 		public AudioClip attackSound1;						//First of two audio clips to play when attacking the player.
-		public AudioClip attackSound2;						//Second of two audio clips to play when attacking the player.
-		
-		
+		public AudioClip attackSound2;                      //Second of two audio clips to play when attacking the player.
+		[SerializeField] private int maxTurnToSkip = 3;
+
 		private Animator animator;							//Variable of type Animator to store a reference to the enemy's Animator component.
-		private Transform target;							//Transform to attempt to move toward each turn.
-		private bool skipMove;								//Boolean to determine whether or not enemy should skip a turn or move this turn.
+		private Transform target;                           //Transform to attempt to move toward each turn.
+		private List<Transform> characters = new List<Transform>();
+		private int turnToSkip;								//Boolean to determine whether or not enemy should skip a turn or move this turn.
 		
 		
 		//Start overrides the virtual Start function of the base class.
@@ -25,9 +28,14 @@ namespace Completed
 			
 			//Get and store a reference to the attached Animator component.
 			animator = GetComponent<Animator> ();
-			
+
 			//Find the Player GameObject using it's tag and store a reference to its transform component.
-			target = GameObject.FindGameObjectWithTag ("Player").transform;
+			GameObject[] g = GameObject.FindGameObjectsWithTag("Player");
+			for (int i = 0; i < g.Length; i++)
+			{
+				characters.Add(g[i].transform);
+			}
+			//target = GameObject.FindGameObjectWithTag ("Player").transform;
 			
 			//Call the start function of our base class MovingObject.
 			base.Start ();
@@ -39,24 +47,34 @@ namespace Completed
 		protected override void AttemptMove <T> (int xDir, int yDir)
 		{
 			//Check if skipMove is true, if so set it to false and skip this turn.
-			if(skipMove)
+			if(turnToSkip > 0)
 			{
-				skipMove = false;
+				turnToSkip--;
 				return;
 				
 			}
 			
 			//Call the AttemptMove function from MovingObject.
 			base.AttemptMove <T> (xDir, yDir);
-			
+
 			//Now that Enemy has moved, set skipMove to true to skip next move.
-			skipMove = true;
+			turnToSkip = maxTurnToSkip;
 		}
 		
 		
 		//MoveEnemy is called by the GameManger each turn to tell each Enemy to try to move towards the player.
 		public void MoveEnemy ()
 		{
+			float maxDistance = Mathf.Infinity;
+			foreach (Transform c in characters)
+			{
+				if ((c.position - transform.position).magnitude < maxDistance)
+				{
+					maxDistance = (c.position - transform.position).magnitude;
+					target = c;
+				}
+			}
+
 			//Declare variables for X and Y axis move directions, these range from -1 to 1.
 			//These values allow us to choose between the cardinal directions: up, down, left and right.
 			int xDir = 0;
